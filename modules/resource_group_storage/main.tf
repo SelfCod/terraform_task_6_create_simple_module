b635c5e0edcebda4d62bbeb229414d8596bd9b08
Yuriy P <xxxx@yy.com>
2025-04-30 00:22:59 +0300

    Added module

---
+++ modules/resource_group_storage/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = secrets.subscription_id
+}
+
+module "resource_group_storage" {
+  source   = "urapikh/resource_group_storage/azurerm"
+  version  = "1.0.0"
+  prefix   = "task6"
+  location = "West Europe"
+}
